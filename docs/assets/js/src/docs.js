8531ae81ac24d545c3d4aa9b7c13bbb95103086b
mbrambilla <mbrambilla@cast.org>
2016-07-18 10:37:54 -0400

    Docs - supporting CSS/JS for player example

--- docs/assets/js/src/docs.js
+++ docs/assets/js/src/docs.js
@@ -108,6 +108,12 @@ function rgb2hex(rgb) {
     return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 
+// Direction for player dropdown menus
+$(document, '[data-cfw="player"]').on('ready.cfw.player', function(e) {
+    $(e.target).closest('[data-cfw="player"]').find('.player-caption-wrapper').addClass('dropup dropdown-menu-left');
+    $(e.target).closest('[data-cfw="player"]').find('.player-script-wrapper').addClass('dropup dropdown-menu-left');
+});
+
 $(window).ready(function() {
     addAnchors();
     addClipboard();
