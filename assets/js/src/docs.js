019cc04c51d154e7b9484a8c9c0f07154c2e2b4a
mbrambilla <mbrambilla@cast.org>
2016-10-13 10:05:34 -0400

    Release v1.3.0

--- assets/js/src/docs.js
+++ assets/js/src/docs.js
@@ -1,5 +1,5 @@
 /*!
- * Figuration (v1.2.0)
+ * Figuration (v1.3.0)
  * http://figuration.org
  * Copyright 2013-2016 CAST, Inc.
  * Licensed under MIT (https://github.com/cast-org/figuration/blob/master/LICENSE)
