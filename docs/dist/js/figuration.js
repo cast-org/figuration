13c838f43354a5435745467190a255630cc83657
mbrambilla <mbrambilla@cast.org>
2017-01-13 12:22:17 -0500

    grunt

--- docs/dist/js/figuration.js
+++ docs/dist/js/figuration.js
@@ -4191,38 +4191,78 @@ if (typeof jQuery === 'undefined') {
 
     var CFW_Widget_Button = function(element) {
         this.$element = $(element);
+        this.$parent = this.$element.closest('[data-cfw="buttons"]');
+
+        this._init();
     };
 
     CFW_Widget_Button.prototype = {
+        _init : function() {
+            var $selfRef = this;
+
+            var $input = this.$element.find('input').first();
+            if ($input.length) {
+                if ($input.prop('checked')) {
+                    this.$element.addClass('active');
+                } else {
+                    this.$element.removeClass('active');
+                }
+            }
+            this.$element.attr('aria-pressed', this.$element.hasClass('active'));
+
+            // Event handlers
+            this.$element
+                .on('click.cfw.button', function(e) {
+                    var $btn = $(this);
+
+                    $selfRef.toggle();
+
+                    if (!$(e.target).is('input')) {
+                        // Prevent double click on radios, and the double selections (so cancellation) on checkboxes
+                        e.preventDefault();
+                        // The target component still receive the focus
+                        if ($btn.is('input,button')) {
+                            $btn.trigger('focus');
+                        } else {
+                            $btn.find('input:visible,button:visible').first().trigger('focus');
+                        }
+                    }
+                });
+            if ($input.length) {
+                this.$element.on('focusin.cfw.button focusout.cfw.button', function(e) {
+                    $(this).toggleClass('focus', /^focus(in)?$/.test(e.type));
+                });
+            }
+        },
 
         toggle : function() {
             var changed = true;
-            var $parent = this.$element.closest('[data-cfw="buttons"]');
 
-            if ($parent.length) {
+            if (this.$parent.length) {
                 var $input = this.$element.find('input');
-                if ($input.prop('type') == 'radio') {
-                    if ($input.prop('checked')) {
-                        changed = false;
+                if ($input.length) {
+                    if ($input.prop('type') == 'radio') {
+                        if ($input.prop('checked') && this.$element.hasClass('active')) {
+                            changed = false;
+                        } else {
+                            this.$parent.find('.active')
+                                .removeClass('active')
+                                .attr('aria-pressed', false);
+                        }
                     }
-                    $parent.find('.active').removeClass('active');
-                    this.$element.addClass('active');
-                } else if ($input.prop('type') == 'checkbox') {
-                    if (($input.prop('checked')) !== this.$element.hasClass('active')) {
-                        changed = false;
+
+                    if (changed) {
+                        $input.prop('checked', !this.$element.hasClass('active'));
+                        $input.trigger('change');
                     }
-                    this.$element.toggleClass('active');
                 }
-                $input.prop('checked', this.$element.hasClass('active'));
-                if (changed) {
-                    $input.trigger('change');
-                }
-            } else {
+            }
+
+            if (changed) {
                 this.$element.attr('aria-pressed', !this.$element.hasClass('active'));
                 this.$element.toggleClass('active');
             }
         }
-
     };
 
     function Plugin(option) {
@@ -4231,42 +4271,22 @@ if (typeof jQuery === 'undefined') {
             var data = $this.data('cfw.button');
             var options = typeof option === 'object' && option;
 
-            if (!data) {
-                $this.data('cfw.button', (data = new CFW_Widget_Button(this, options)));
+            // Check to see if group
+            if ($this.is('[data-cfw="buttons"]')) {
+                // Pass through to buttons
+                $this.find('.btn').CFW_Button(option);
+            } else {
+                // Operate on independent buttons
+                if (!data) {
+                    $this.data('cfw.button', (data = new CFW_Widget_Button(this, options)));
+                }
+                if (option == 'toggle') data.toggle();
             }
-            data.toggle();
         });
     }
 
     $.fn.CFW_Button = Plugin;
     $.fn.CFW_Button.Constructor = CFW_Widget_Button;
-
-    // API
-    // ===
-    $(document)
-        .on('click.cfw.button', '[data-cfw^="button"]', function(e) {
-            var $btn = $(e.target).closest('.btn');
-
-            Plugin.call($btn, 'toggle');
-
-            if (!($(e.target).is('input[type="radio"], input[type="checkbox"]'))) {
-                // Prevent double click on radios, and the double selections (so cancellation) on checkboxes
-                e.preventDefault();
-                // The target component still receive the focus
-                if ($btn.is('input,button')) {
-                    $btn.trigger('focus');
-                } else {
-                    $btn.find('input:visible,button:visible').first().trigger('focus');
-                }
-            }
-
-            if (!($(e.target).is('input[type="radio"]') || $(e.target).is('input[type="checkbox"]'))) {
-                e.preventDefault();
-            }
-        })
-        .on('focus.cfw.button blur.cfw.button', '[data-cfw^="button"]', function(e) {
-            $(e.target).closest('.btn').toggleClass('focus', /^focus(in)?$/.test(e.type));
-        });
 
 })(jQuery);
 
@@ -6718,6 +6738,9 @@ if (typeof jQuery === 'undefined') {
         var $scope = $(this);
         if (!$scope) { $scope = $(document.body); }
 
+        $('[data-cfw^="button"]', $scope).each(function() {
+            $(this).CFW_Button();
+        });
         $('[data-cfw="collapse"]', $scope).each(function() {
             $(this).CFW_Collapse();
         });
