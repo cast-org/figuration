1bfaab2e17f57f070c2aade9dd19f644d00b108c
mbrambilla <mbrambilla@cast.org>
2017-07-03 10:37:33 -0400

    cleanup comments

--- grunt/autoprefixer-settings.js
+++ grunt/autoprefixer-settings.js
@@ -6,14 +6,12 @@ module.exports = {
         'Firefox >= 45',
         // Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
         // NOT the Edge app version shown in Edge's "About" screen.
-        // For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
         // See also https://github.com/Fyrd/caniuse/issues/1928
         'Edge >= 12',
         'Explorer >= 10',
         // Out of leniency, we prefix these 1 version further back than the official policy.
         'iOS >= 9',
         'Safari >= 9',
-        // The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
         'Android >= 4.4'
     ]
 };
