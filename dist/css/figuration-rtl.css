114769eab8444ae9cff68fcda7abdafc5fc82825
mbrambilla <mbrambilla@cast.org>
2017-11-06 12:07:31 -0500

    Release v3.0.3 docs

--- dist/css/figuration-rtl.css
+++ dist/css/figuration-rtl.css
@@ -1,5 +1,5 @@
 /*!
- * Figuration (v3.0.2)
+ * Figuration (v3.0.3)
  * http://figuration.org
  * Copyright 2013-2017 CAST, Inc.
  * Licensed under MIT (https://github.com/cast-org/figuration/blob/master/LICENSE)
