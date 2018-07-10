11baf8341f15de887082db4228395e5844840c00
mbrambilla <mbrambilla@cast.org>
2018-07-10 17:19:40 -0400

    grunt

--- dist/js/figuration.js
+++ dist/js/figuration.js
@@ -4572,12 +4572,18 @@ if (typeof jQuery === 'undefined') {
             this.$element
                 .data('cfw.alert', this)
                 .on('click.cfw.alert', function(e) {
-                    e.preventDefault();
-                    $selfRef.close();
+                    $selfRef.handleClose(e);
                 });
 
             this.$parent
                 .CFW_trigger('init.cfw.alert');
+        },
+
+        handleClose : function(e) {
+            e.preventDefault();
+            if ($(e.currentTarget).closest(dismiss, this.$element).not('.disabled, :disabled').length) {
+                this.close();
+            }
         },
 
         close : function(e) {
@@ -4655,8 +4661,8 @@ if (typeof jQuery === 'undefined') {
     // API
     // ===
     if (typeof CFW_API === 'undefined' || CFW_API !== false) {
-        $(document).on('click.cfw.alert', dismiss, function() {
-            $(this).CFW_Alert('close');
+        $(document).on('click.cfw.alert', dismiss, function(e) {
+            $(this).CFW_Alert('handleClose', e);
         });
     }
 }(jQuery));
