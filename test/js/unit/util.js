0d9d6d0e5416f188c1c516626611a1d58c1b8a49
mbrambilla <mbrambilla@cast.org>
2018-09-05 14:56:09 -0400

    Util: fix transition duration check and remove selector escaping

--- test/js/unit/util.js
+++ test/js/unit/util.js
@@ -4,7 +4,7 @@ $(function() {
     QUnit.module('util');
 
     QUnit.test('CFW_getSelectorFromElement should return the correct element', function(assert) {
-        assert.expect(5);
+        assert.expect(2);
 
         var $el = $('<div data-cfw-test-target="body"></div>').appendTo($('#qunit-fixture'));
         assert.strictEqual($el.CFW_getSelectorFromElement('test'), 'body');
@@ -12,17 +12,48 @@ $(function() {
         // not found element
         var $el2 = $('<div data-cfw-test-target="#fakeDiv"></div>').appendTo($('#qunit-fixture'));
         assert.strictEqual($el2.CFW_getSelectorFromElement('test'), null);
+    });
 
-        // should escape ID and find the correct element
-        var $el3 = $('<div data-cfw-test-target="#collapse:Example"></div>').appendTo($('#qunit-fixture'));
-        $('<div id="collapse:Example"></div>').appendTo($('#qunit-fixture'));
-        assert.strictEqual($el3.CFW_getSelectorFromElement('test'), '#collapse\\:Example');
+    QUnit.test('CFW_transitionCssDuration should accept transition durations in milliseconds', function(assert) {
+        assert.expect(1);
+        var $div = $('<div style="transition: all 300ms ease-out;"></div>').appendTo($('#qunit-fixture'));
 
-        // if $.escapeSelector doesn't exist in older jQuery versions (< 3)
-        var tmpEscapeSelector = $.escapeSelector;
-        delete $.escapeSelector;
-        assert.ok(typeof $.escapeSelector === 'undefined', '$.escapeSelector undefined');
-        assert.strictEqual($el3.CFW_getSelectorFromElement('test'), '#collapse\\:Example');
-        $.escapeSelector = tmpEscapeSelector;
+        assert.strictEqual($().CFW_transitionDuration($div), 300);
+    });
+
+    QUnit.test('CFW_transitionCssDuration should accept transition durations in seconds', function(assert) {
+        assert.expect(1);
+        var $div = $('<div style="transition: all .4s ease-out;"></div>').appendTo($('#qunit-fixture'));
+
+        assert.strictEqual($().CFW_transitionDuration($div), 400);
+    });
+
+    QUnit.test('CFW_transitionCssDuration should get the longest transition duration if multiple transition durations are defined', function(assert) {
+        assert.expect(1);
+        var $div = $('<div style="transition: transform .1s ease-out, opacity .3s;"></div>').appendTo($('#qunit-fixture'));
+
+        assert.strictEqual($().CFW_transitionDuration($div), 300);
+    });
+
+    QUnit.test('CFW_transitionCssDuration should get the longest transition duration if multiple transition elements are defined', function(assert) {
+        assert.expect(1);
+        var $div0 = $('<div style="transition: transform .1s ease-out, opacity .2s"></div>').appendTo($('#qunit-fixture'));
+        var $div1 = $('<div style="transition: transform .1s ease-out, opacity .3s;"></div>').appendTo($('#qunit-fixture'));
+        var $divs = $div0.add($div1);
+        assert.strictEqual($().CFW_transitionDuration($divs), 300);
+    });
+
+    QUnit.test('CFW_transitionCssDuration should return 0 if transition duration is not defined', function(assert) {
+        assert.expect(1);
+        var $div = $('<div></div>').appendTo($('#qunit-fixture'));
+
+        assert.strictEqual($().CFW_transitionDuration($div), 0);
+    });
+
+    QUnit.test('CFW_transitionCssDuration should return 0 if element is not found in DOM', function(assert) {
+        assert.expect(1);
+        var $div = $('#fake-id');
+
+        assert.strictEqual($().CFW_transitionDuration($div), 0);
     });
 });
