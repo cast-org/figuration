6c7c69decc995473d12863c67ab1ebd31f8c53ef
Matthew Brambilla <mbrambilla@cast.org>
2020-05-13 14:30:39 -0400

    grunt

--- dist/js/figuration.js
+++ dist/js/figuration.js
@@ -1714,18 +1714,18 @@ if (typeof jQuery === 'undefined') {
                 return;
             }
 
-            var $previous = this.$navElm.find('.active').last();
+            var $previous = this.$navElm.find('.active');
             var eventHideResult;
             var eventShowResult;
 
             if ($previous.length) {
-                eventHideResult = $previous.CFW_trigger('beforeHide.cfw.tab', {
+                eventHideResult = $previous.last().CFW_trigger('beforeHide.cfw.tab', {
                     relatedTarget: this.$element[0]
                 });
             }
 
             eventShowResult = this.$element.CFW_trigger('beforeShow.cfw.tab', {
-                relatedTarget: $previous[0]
+                relatedTarget: $previous.last()[0]
             });
 
             if (!eventHideResult || !eventShowResult) {
@@ -1737,9 +1737,6 @@ if (typeof jQuery === 'undefined') {
                     .attr({
                         'tabindex': -1,
                         'aria-selected': 'false'
-                    })
-                    .CFW_trigger('afterHide.cfw.tab', {
-                        relatedTarget: this.$element[0]
                     });
             }
 
@@ -1748,8 +1745,7 @@ if (typeof jQuery === 'undefined') {
                 'aria-selected': 'true'
             });
 
-            this._activateTab(this.$element, this.$navElm, false, $previous);
-            this._activateTab(this.$target, this.$target.parent(), true, $previous);
+            this._activateTab();
         },
 
         animEnable : function() {
@@ -1791,38 +1787,44 @@ if (typeof jQuery === 'undefined') {
             nextTab.CFW_Tab('show').trigger('focus');
         },
 
-        _activateTab : function($node, container, isPanel, $previous) {
+        _activateTab : function() {
             var $selfRef = this;
-            var $prevActive = container.find('.active');
-            var doTransition = false;
-            if (isPanel && this.settings.animate) {
-                doTransition = true;
+            var $items = this.$navElm.find('[role="tab"]');
+            var $previous = this.$navElm.find('[role="tab"].active');
+
+            $items.removeClass('active');
+            $items.each(function() {
+                var $pane = $(this).data('cfw.tab').$target;
+                $pane.removeClass('active in');
+            });
+
+            if (this.settings.animate) {
+                this.animEnable();
+            } else {
+                this.animDisable();
             }
 
-            if (doTransition) {
-                $.CFW_reflow($node[0]); // Reflow for transition
-                $node.addClass('in');
-            } else {
-                if (isPanel) {
-                    $selfRef.settings.animate = false;
-                }
-                $node.removeClass('fade');
-            }
+            this.$element.addClass('active');
+            this.$target.addClass('active');
 
             var complete = function() {
-                $prevActive.removeClass('active in');
-                $node.addClass('active');
-
-                if (isPanel) {
-                    $selfRef.$element.CFW_trigger('afterShow.cfw.tab', {
-                        relatedTarget: $previous[0]
-                    });
-                    $node.CFW_mutateTrigger();
-                    $prevActive.CFW_mutateTrigger();
-                }
+                $previous.last().CFW_trigger('afterHide.cfw.tab', {
+                    relatedTarget: $selfRef.$element[0]
+                });
+                $selfRef.$element.CFW_trigger('afterShow.cfw.tab', {
+                    relatedTarget: $previous.last()[0]
+                });
+                $selfRef.$element.CFW_mutateTrigger();
+                $selfRef.$target.CFW_mutateTrigger();
             };
 
-            $node.CFW_transition(null, complete);
+            if (this.settings.animate) {
+                this.$target.CFW_transition(null, complete);
+                $.CFW_reflow(this.$target); // Reflow for transition
+                this.$target.addClass('in');
+            } else {
+                complete();
+            }
         },
 
         dispose : function() {
@@ -5023,12 +5025,12 @@ if (typeof jQuery === 'undefined') {
                         $selfRef._equalize();
                     }
                 });
-            } else {
-                this.$target.CFW_mutationListen();
-                this.$element
-                    .attr('data-cfw-mutate', '')
-                    .on('mutate.cfw.mutate', this._equalize.bind(this));
             }
+
+            this.$target.CFW_mutationListen();
+            this.$element
+                .attr('data-cfw-mutate', '')
+                .on('mutate.cfw.mutate', this._equalize.bind(this));
 
             this.$window.on('resize.cfw.equalize.' + this.instance, $.CFW_throttle(this._equalize.bind(this), this.settings.throttle));
 
