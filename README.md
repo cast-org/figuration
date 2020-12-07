f9d3bfdba2bd9249b7e73a0aa93bc05ce3237e3f
Matthew Brambilla <mbrambilla@cast.org>
2020-12-07 16:07:11 -0500

    v4.1.0 update

--- README.md
+++ README.md
@@ -20,18 +20,18 @@ CAST Figuration: A feature rich, responsive, mobile first, accessible, front-end
 ## Status
 
 **Latest Release**
-[![npm version](https://img.shields.io/npm/v/figuration.svg)](https://www.npmjs.com/package/figuration)
-[![CSS size](http://img.badgesize.io/cast-org/figuration/v4.0.0-beta.4/dist/css/figuration.min.css?label=CSS+size)](https://github.com/cast-org/figuration/tree/v4.0.0-beta.4/dist/css/figuration.min.css)
-[![CSS gzip size](http://img.badgesize.io/cast-org/figuration/v4.0.0-beta.4/dist/css/figuration.min.css?compression=gzip&label=CSS+gzip+size)](https://github.com/cast-org/figuration/tree/v4.0.0-beta.4/dist/css/figuration.min.css)
-[![JS size](http://img.badgesize.io/cast-org/figuration/v4.0.0-beta.4/dist/js/figuration.min.js?label=JS+size)](https://github.com/cast-org/figuration/tree/v4.0.0-beta.4/dist/js/figuration.min.js)
-[![JS gzip size](http://img.badgesize.io/cast-org/figuration/v4.0.0-beta.4/dist/js/figuration.min.js?compression=gzip&label=JS+gzip+size)](https://github.com/cast-org/figuration/tree/v4.0.0-beta.4/dist/js/figuration.min.js)
+[![npm version](https://img.shields.io/npm/v/figuration)](https://www.npmjs.com/package/figuration)
+[![CSS size](http://img.badgesize.io/cast-org/figuration/master/dist/css/figuration.min.css?label=CSS%20size)](https://github.com/cast-org/figuration/tree/master/dist/css/figuration.min.css)
+[![CSS gzip size](http://img.badgesize.io/cast-org/figuration/master/dist/css/figuration.min.css?compression=gzip&label=CSS%20gzip%20size)](https://github.com/cast-org/figuration/tree/master/dist/css/figuration.min.css)
+[![JS size](http://img.badgesize.io/cast-org/figuration/master/dist/js/figuration.min.js?label=JS%20size)](https://github.com/cast-org/figuration/tree/master/dist/js/figuration.min.js)
+[![JS gzip size](http://img.badgesize.io/cast-org/figuration/master/dist/js/figuration.min.js?compression=gzip&label=JS%20gzip%20size)](https://github.com/cast-org/figuration/tree/master/dist/js/figuration.min.js)
 
 **Dependencies**
-[![peerDependencies Status](https://david-dm.org/cast-org/figuration/v4-dev/peer-status.svg)](https://david-dm.org/cast-org/figuration/v4-dev?type=peer)
-[![devDependencies Status](https://david-dm.org/cast-org/figuration/v4-dev/dev-status.svg)](https://david-dm.org/cast-org/figuration/v4-dev?type=dev)
+[![peerDependencies Status](https://david-dm.org/cast-org/figuration/peer-status.svg)](https://david-dm.org/cast-org/figuration/?type=peer)
+[![devDependencies Status](https://david-dm.org/cast-org/figuration/dev-status.svg)](https://david-dm.org/cast-org/figuration/?type=dev)
 
 **Development Build Status**
-[![Build Status](https://img.shields.io/travis/cast-org/figuration/master.svg)](https://travis-ci.org/cast-org/figuration)
+[![Build Status](https://img.shields.io/travis/cast-org/figuration/master)](https://travis-ci.com/cast-org/figuration)
 [![Sauce Labs Test Status](https://saucelabs.com/browser-matrix/figuration.svg)](https://saucelabs.com/u/figuration)
 
 
@@ -39,14 +39,12 @@ CAST Figuration: A feature rich, responsive, mobile first, accessible, front-end
 
 Several quick start options are available:
 
-Nothing has been released yet, as this is a development version.
-
-- [Download the latest release.](https://github.com/cast-org/figuration/archive/v4.0.0-beta.4.zip)
+- [Download the latest release.](https://github.com/cast-org/figuration/archive/v4.1.0.zip)
 - Clone the repo: `git clone https://github.com/cast-org/figuration.git`
-- Install with [npm](https://www.npmjs.com/): `npm install figuration@4.0.0-beta.4`
-- Install with [yarn](https://yarnpkg.com/): `yarn add figuration@4.0.0-beta.4`
+- Install with [npm](https://www.npmjs.com/): `npm install figuration@4.1.0`
+- Install with [yarn](https://yarnpkg.com/): `yarn add figuration@4.1.0`
 
-Read the [Get Started page](http://figuration.org/get-started/quick-start/) for information on the framework, templates, and examples.
+Read the [Get Started page](http://figuration.org/4.1/get-started/quick-start/) for information on the framework, templates, and examples.
 
 
 ### What's Included
@@ -71,7 +69,7 @@ We provide compiled CSS and JS (`figuration.*`), as well as compiled and minifie
 
 ## Bugs and Feature Requests
 
-Have a bug or a feature request? Please first read the [issue guidelines](https://github.com/cast-org/figuration/tree/v4-dev/CONTRIBUTING.md#using-the-issue-tracker) and search for existing and closed issues. If your problem or idea is not addressed yet, [please open a new issue](https://github.com/cast-org/figuration/issues/new).
+Have a bug or a feature request? Please first read the [issue guidelines](https://github.com/cast-org/figuration/tree/master/CONTRIBUTING.md#using-the-issue-tracker) and search for existing and closed issues. If your problem or idea is not addressed yet, [please open a new issue](https://github.com/cast-org/figuration/issues/new).
 
 
 ## Documentation
@@ -81,22 +79,21 @@ Figurations's documentation, included in this repo in the root directory, is bui
 
 ### Running Documentation Locally
 
-1. Run through the [tooling setup](https://github.com/cast-org/figuration/tree/v4-dev/docs/get-started/build-tools.md#tooling-setup) to install the development dependencies.
-2. Run `npm install` to install Node.js dependencies.
-4. Run `grunt` (or a specific set of Grunt tasks) to rebuild distributed CSS and JavaScript files, as well as our docs assets.
-5. From the root `/figuration` directory, run `npm run docs-serve` in the command line.
-6. Open `http://localhost:9001` in your browser, and enjoy.
+1. Run `npm install` to install Node.js dependencies.
+2. Run `grunt dist` (or a specific set of Grunt tasks) to rebuild distributed CSS and JavaScript files, as well as our docs assets.
+3. From the root `/figuration` directory, run `npm run docs-serve` in the command line.
+4. Open `http://localhost:9001` in your browser, and enjoy.
 
 Learn more about using Eleventy by reading its [documentation](https://www.11ty.io/docs/).
 
 
 ## Contributing
 
-Please read through our [contributing guidelines](https://github.com/cast-org/figuration/tree/v4-dev/CONTRIBUTING.md). Included are directions for opening issues, coding standards, and notes on development.
+Please read through our [contributing guidelines](https://github.com/cast-org/figuration/tree/master/CONTRIBUTING.md). Included are directions for opening issues, coding standards, and notes on development.
 
-If your pull request contains JavaScript patches or features, you must include [relevant unit tests](https://github.com/cast-org/figuration/tree/v4-dev/js/tests). All HTML and CSS should conform to the coding standards.
+If your pull request contains JavaScript patches or features, you must include [relevant unit tests](https://github.com/cast-org/figuration/tree/master/js/tests). All HTML and CSS should conform to the coding standards.
 
-Editor preferences are available in the [editor config](https://github.com/cast-org/figuration/tree/v4-dev/.editorconfig) for easy use in common text editors. Read more and download plugins at <https://editorconfig.org>.
+Editor preferences are available in the [editor config](https://github.com/cast-org/figuration/tree/master/.editorconfig) for easy use in common text editors. Read more and download plugins at <https://editorconfig.org>.
 
 
 ## Community
@@ -118,4 +115,4 @@ See [the Releases section of our GitHub project](https://github.com/cast-org/fig
 
 ## Copyright and License
 
-Code and documentation copyright 2013-2020 [CAST, Inc.](http://www.cast.org/) Code released under [the MIT license](https://github.com/cast-org/figuration/tree/v4-dev/LICENSE). Docs released under [Creative Commons](https://github.com/cast-org/figuration/tree/v4-dev/docs/LICENSE).
+Code and documentation copyright 2013-2020 [CAST, Inc.](http://www.cast.org/) Code released under [the MIT license](https://github.com/cast-org/figuration/tree/master/LICENSE). Docs released under [Creative Commons](https://github.com/cast-org/figuration/tree/master/docs/LICENSE).
---
+++ assets/4.1/js/src/docs.js
@@ -0,0 +1,251 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+/* eslint-disable no-alert, no-magic-numbers, no-new-func */
+/* global ClipboardJS, Holder */
+
+/*!
+ * Figuration (v4.1.0)
+ * http://figuration.org
+ * Copyright 2013-2020 CAST, Inc.
+ * Licensed under MIT (https://github.com/cast-org/figuration/blob/master/LICENSE)
+ */
+
+// Intended to prevent false-positive bug reports about stuff not working properly in old versions of IE due to folks testing using IE's unreliable emulation modes.
+(function() {
+    'use strict';
+
+    var emulatedIEMajorVersion = function() {
+        var groups = /MSIE ([0-9.]+)/.exec(window.navigator.userAgent);
+        if (groups === null) {
+            return null;
+        }
+        var ieVersionNum = parseInt(groups[1], 10);
+        var ieMajorVersion = Math.floor(ieVersionNum);
+        return ieMajorVersion;
+    };
+
+    var actualNonEmulatedIEMajorVersion = function() {
+        // Detects the actual version of IE in use, even if it's in an older-IE emulation mode.
+        // IE JavaScript conditional compilation docs: https://msdn.microsoft.com/library/121hztk3%28v=vs.94%29.aspx
+        // @cc_on docs: https://msdn.microsoft.com/library/8ka90k2e%28v=vs.94%29.aspx
+        var jscriptVersion = new Function('/*@cc_on return @_jscript_version; @*/')();
+        if (typeof jscriptVersion === 'undefined') {
+            return 11; // IE11+ not in emulation mode
+        }
+        if (jscriptVersion < 9) {
+            return 8; // IE8 (or lower; haven't tested on IE<8)
+        }
+        return jscriptVersion; // IE9 or IE10 in any mode, or IE11 in non-IE11 mode
+    };
+
+    var ua = window.navigator.userAgent;
+    if (ua.indexOf('Opera') > -1 || ua.indexOf('Presto') > -1) {
+        return; // Opera, which might pretend to be IE
+    }
+    var emulated = emulatedIEMajorVersion();
+    if (emulated === null) {
+        return; // Not IE
+    }
+
+    var nonEmulated = actualNonEmulatedIEMajorVersion();
+    if (emulated !== nonEmulated) {
+        window.alert('WARNING: You appear to be using IE' + nonEmulated + ' in IE' + emulated + ' emulation mode.\nIE emulation modes can behave significantly differently from ACTUAL older versions of IE.\nPLEASE DON\'T FILE CAST FIGURATION BUGS based on testing in IE emulation modes!');
+    }
+}());
+
+(function($) {
+    'use strict';
+
+    Holder.addTheme('gray', {
+        bg: '#999',
+        fg: '#111',
+        size: 12,
+        font: 'sans-serif',
+        fontweight: 'normal'
+    });
+
+    var addClipboard = function() {
+        // Insert copy to clipboard button before .highlight
+        // $('.highlight').each(function() {
+        $('pre[class*="language-"]').each(function() {
+            var btnHtml = '<div class="cf-clipboard"><button type="button" class="btn btn-xsmall btn-secondary btn-clipboard print-none" title="Copy to clipboard">Copy</button></div>';
+            $(this).before(btnHtml);
+        });
+
+        $('.btn-clipboard')
+            .CFW_Tooltip({
+                animate: false
+            });
+
+        var clipboard = new ClipboardJS('.btn-clipboard', {
+            target: function(trigger) {
+                return trigger.parentNode.nextElementSibling;
+            }
+        });
+
+        clipboard.on('success', function(e) {
+            $(e.trigger)
+                .CFW_Tooltip('hide')
+                .attr('data-cfw-tooltip-original-title', 'Copied!')
+                .CFW_Tooltip('show')
+                .attr('data-cfw-tooltip-original-title', 'Copy to clipboard');
+
+            e.clearSelection();
+        });
+
+        clipboard.on('error', function(e) {
+            var modifierKey = /Mac/i.test(navigator.userAgent) ? '\u2318' : 'Ctrl-';
+            var fallbackMsg = 'Press ' + modifierKey + 'C to copy';
+
+            $(e.trigger)
+                .CFW_Tooltip('hide')
+                .attr('data-cfw-tooltip-original-title', fallbackMsg)
+                .CFW_Tooltip('show')
+                .attr('data-cfw-tooltip-original-title', 'Copy to clipboard');
+        });
+    };
+
+    var toplinkAffix = function() {
+        var $toplink = $('.toplink');
+
+        $toplink.CFW_Affix({
+            top: function() {
+                return $('.cf-title').offset().top;
+            },
+            bottom: function() {
+                return $('footer').outerHeight();
+            }
+        });
+        $(window).scroll();
+    };
+
+    var rgb2hex = function(rgb) {
+        if (/^#[0-9A-F]{6}$/i.test(rgb)) { return rgb; }
+
+        rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        var hex = function(x) {
+            return ('0' + parseInt(x, 10).toString(16)).slice(-2);
+        };
+        return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+    };
+
+    var paletteHex = function() {
+        var $items = $('.palette-base, .palette-item');
+        var $this = null;
+        var color = '';
+        if ($items.length > 0) {
+            $items.each(function() {
+                $this = $(this);
+                color = rgb2hex($this.css('background-color'));
+                $this.append('<span class="float-end">' + color + '</span>');
+            });
+        }
+    };
+
+    var pageToc = function() {
+        var $toc = $('.cf-toc').eq(0);
+        if (!$toc.length) { return; }
+
+        $toc.attr('aria-label', 'Page');
+
+        // Add scrollspy here - using <body> data attributes comes too early
+        $('body').CFW_Scrollspy({
+            offset: 75,
+            target: '.cf-toc ol'
+        });
+    };
+
+    var docsDirection = function() {
+        var fileRename = function(id, filename) {
+            var $node = $('#' + id);
+            var path = $node.attr('href');
+            var isMin = path.indexOf('.min.css') >= 0;
+            var re = /^(.*\/)?[^/]+\.(css)$/i;
+            var repStr = '$1' + filename;
+            path = path.replace(re, repStr);
+            path += isMin ? '.min.css' : '.css';
+            $node.attr('href', path);
+        };
+
+        var setLTR = function(doReset) {
+            $('#dir-ltr').closest('ul').find('.active').removeClass('active').removeAttr('aria-current');
+            $('#dir-ltr').addClass('active').attr('aria-current', 'true');
+            $('html').removeAttr('dir');
+            if (doReset) {
+                fileRename('cssCore', 'figuration');
+                fileRename('cssDocs', 'docs');
+            }
+            document.cookie = 'docsDir=;path=/;';
+        };
+
+        var setRTL = function() {
+            $('#dir-rtl').closest('ul').find('.active').removeClass('active').removeAttr('aria-current');
+            $('#dir-rtl').addClass('active').attr('aria-current', 'true');
+            $('html').attr('dir', 'rtl');
+            fileRename('cssCore', 'figuration-rtl');
+            fileRename('cssDocs', 'docs-rtl');
+            document.cookie = 'docsDir=rtl;path=/;';
+        };
+
+        var getCookie = function(cname) {
+            var name = cname + '=';
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var ca = decodedCookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) === ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) === 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return '';
+        };
+
+        $('#dir-ltr').on('click', function(e) {
+            e.preventDefault();
+            setLTR(true);
+        });
+        $('#dir-rtl').on('click', function(e) {
+            e.preventDefault();
+            setRTL();
+        });
+
+        // Check on load
+        // var settings = document.cookie;
+        if (getCookie('docsDir') === 'rtl') {
+            setRTL();
+        } else {
+            setLTR(false);
+        }
+    };
+
+    // Direction for player dropdown menus
+    $(document, '[data-cfw="player"]').on('ready.cfw.player', function(e) {
+        $(e.target).closest('[data-cfw="player"]').find('.dropdown-menu').addClass('dropup dropreverse');
+    });
+
+    $(window).ready(function() {
+        addClipboard();
+        toplinkAffix();
+        paletteHex();
+        pageToc();
+        docsDirection();
+
+        // Indeterminate checkbox example
+        $('.cf-example-indeterminate [type="checkbox"]').prop('indeterminate', true);
+
+        // Disable empty links in docs examples
+        $('.cf-content [href="#"]').on('click', function(e) {
+            e.preventDefault();
+        });
+
+        // Toggle animated progress bar
+        $('#animToggle').on('click', function() {
+            $('#animTarget').toggleClass('progress-bar-animated');
+        });
+    });
+}(jQuery));
