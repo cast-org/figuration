a6b12ba4bef41382509be6071c769bbea404cb24
Matthew Brambilla <mbrambilla@cast.org>
2022-01-05 21:57:38 -0500

    grunt dist

--- dist/js/figuration.js
+++ dist/js/figuration.js
@@ -1,10 +1,10 @@
 /*!
  * Figuration (v4.2.1)
  * https://figuration.org
- * Copyright 2013-2021 CAST, Inc.
+ * Copyright 2013-2022 CAST, Inc.
  * Licensed under MIT (https://github.com/cast-org/figuration/blob/master/LICENSE)
  * -----
- * Portions Copyright 2011-2021  the Bootstrap Authors and Twitter, Inc.
+ * Portions Copyright 2011-2022  the Bootstrap Authors and Twitter, Inc.
  * Used under MIT License (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
@@ -156,6 +156,193 @@ if (typeof jQuery === 'undefined') {
     };
 
     window.CFW_Backdrop = CFW_Util_Backdrop;
+}(jQuery));
+
+/**
+ * --------------------------------------------------------------------------
+ * Figuration (v4.2.1): util/scrollbar.js
+ * Licensed under MIT (https://github.com/cast-org/figuration/blob/master/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+(function($) {
+    'use strict';
+
+    var SELECTOR_CONTENT_FIXED = '.fixed-top, .fixed-bottom, .is-fixed';
+    var SELECTOR_CONTENT_STICKY = '.sticky-top, .is-sticky';
+    var SELECTOR_CONTENT_SHARED = [SELECTOR_CONTENT_FIXED, SELECTOR_CONTENT_STICKY].join(', ');
+
+    var CFW_Util_Scrollbar = function(options) {
+        this.element = null;
+        this.scrollbarWidth = null;
+        this.settings = $.extend({}, CFW_Util_Scrollbar.DEFAULTS, options);
+
+        this._init();
+    };
+
+    CFW_Util_Scrollbar.DEFAULTS = {
+        rootElement: 'body'
+    };
+
+    CFW_Util_Scrollbar.prototype = {
+        _init : function() {
+            this.element = this._getElement(this.settings.rootElement);
+        },
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth#usage_notes
+        getContainerWidth : function() {
+            return this.element === document.body ? window.innerWidth : this.element.getBoundingClientRect().width;
+        },
+
+        getContentWidth : function() {
+            return this.element === document.body ? document.documentElement.clientWidth : this.element.clientWidth;
+        },
+
+        getScrollbarWidth : function() {
+            return Math.abs(this.getContainerWidth() - this.getContentWidth());
+        },
+
+        getScrollbarSide : function() {
+            // Unable to detect side when 0-width scrollbars (such as mobile)
+            // are found.  So we use 'right` side as default (more common case).
+            var isHTML = this.element === document.documentElement;
+            var isEdge = /edge\/\d+/i.test(navigator.userAgent);
+            var isIE = /(msie|trident)/i.test(navigator.userAgent);
+
+            if (!isHTML || isEdge || isIE) {
+                return $.CFW_isRTL(this.element) ? 'left' : 'right';
+            }
+            return 'right';
+        },
+
+        isOverflowing : function() {
+            return this.getScrollbarWidth() > 0;
+        },
+
+        disable : function() {
+            var $selfRef = this;
+            this.scrollbarWidth = this.getScrollbarWidth();
+
+            var paddingCalc = function(calculatedVal) {
+                var newWidth = calculatedVal + $selfRef.scrollbarWidth;
+                return newWidth + 'px';
+            };
+            var marginCalc = function(calculatedVal) {
+                var newWidth = calculatedVal - $selfRef.scrollbarWidth;
+                return newWidth + 'px';
+            };
+            var side = this.getScrollbarSide();
+
+            // Update rootElement overflow and padding
+            this._disableOverflow();
+            this._setScrollbarAdjustment(this.element, 'padding-' + side, paddingCalc);
+
+            // Update fixed/sticky positioned element padding
+            $(SELECTOR_CONTENT_SHARED).each(function() {
+                if ($selfRef._isFixed(this) || $selfRef._isSticky(this)) {
+                    $selfRef._setScrollbarAdjustment(this, 'padding-' + side, paddingCalc);
+                }
+            });
+
+            // Update sticky positioned element margin
+            $(SELECTOR_CONTENT_STICKY).each(function() {
+                if ($selfRef._isSticky(this)) {
+                    $selfRef._setScrollbarAdjustment(this, 'margin-' + side, marginCalc);
+                }
+            });
+        },
+
+        reset : function() {
+            var $selfRef = this;
+            var side = this.getScrollbarSide();
+            this._resetScrollbarAdjustment(this.element, 'overflow');
+            this._resetScrollbarAdjustment(this.element, 'padding-' + side);
+            $(SELECTOR_CONTENT_SHARED).each(function() {
+                $selfRef._resetScrollbarAdjustment(this, 'padding-' + side);
+            });
+            $(SELECTOR_CONTENT_STICKY).each(function() {
+                $selfRef._resetScrollbarAdjustment(this, 'margin-' + side);
+            });
+            this.scrollbarWidth = null;
+        },
+
+        _setScrollbarAdjustment : function(node, property, callback) {
+            if (node !== this.element && this.getContainerWidth() > node.clientWidth + this.scrollbarWidth) {
+                return;
+            }
+
+            var actualVal = node.style.getPropertyValue(property);
+            var calculatedVal = parseFloat(window.getComputedStyle(node).getPropertyValue(property));
+            if (actualVal) {
+                node.setAttribute('data-cfw-' + property, actualVal);
+            }
+            node.style.setProperty(property, typeof callback === 'function' ? callback(calculatedVal) : callback);
+        },
+
+        _resetScrollbarAdjustment : function(node, property) {
+            var savedVal = this._normalizeData(node.getAttribute('data-cfw-' + property));
+            if (savedVal === null) {
+                node.style.removeProperty(property);
+            }
+            node.removeAttribute('data-cfw-' + property);
+            node.style.setProperty(property, savedVal);
+        },
+
+        _disableOverflow : function() {
+            this._setScrollbarAdjustment(this.element, 'overflow', 'hidden');
+        },
+
+        _isFixed : function(node) {
+            return Boolean(window.getComputedStyle(node).position === 'fixed');
+        },
+
+        _isSticky : function(node) {
+            return Boolean(window.getComputedStyle(node).position === 'sticky');
+        },
+
+        _isElement : function(object) {
+            if (!object || typeof object !== 'object') {
+                return false;
+            }
+            if (typeof object.jquery !== 'undefined') {
+                object = object[0];
+            }
+            return typeof object.nodeType !== 'undefined';
+        },
+
+        _getElement : function(object) {
+            // Check for jQuery object or a node element
+            if (this._isElement(object)) {
+                return object.jquery ? object[0] : object;
+            }
+            if (typeof object === 'string' && object.length > 0) {
+                return document.querySelector(object);
+            }
+            return null;
+        },
+
+        _normalizeData : function(val) {
+            if (val === 'true') {
+                return true;
+            }
+
+            if (val === 'false') {
+                return false;
+            }
+
+            if (val === Number(val).toString()) {
+                return Number(val);
+            }
+
+            if (val === '' || val === 'null') {
+                return null;
+            }
+
+            return val;
+        }
+    };
+
+    window.CFW_Scrollbar = CFW_Util_Scrollbar;
 }(jQuery));
 
 /**
@@ -3748,7 +3935,7 @@ if (typeof jQuery === 'undefined') {
  * Licensed under MIT (https://github.com/cast-org/figuration/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
-/* global CFW_Backdrop */
+/* global CFW_Backdrop, CFW_Scrollbar */
 
 (function($) {
     'use strict';
@@ -3759,10 +3946,9 @@ if (typeof jQuery === 'undefined') {
         this.$target = null;
         this.$dialog = null;
         this._backdrop = null;
+        this._scrollbar = null;
         this.$focusLast = null;
         this.isShown = null;
-        this.scrollbarWidth = 0;
-        this.scrollbarSide = 'right';
         this.fixedContent = '.fixed-top, .fixed-bottom, .is-fixed, .sticky-top';
         this.stickyContent = '.sticky-top';
         this.ignoreBackdropClick = false;
@@ -3791,6 +3977,7 @@ if (typeof jQuery === 'undefined') {
             this.$target = $(selector);
             this.$dialog = this.$target.find('.modal-dialog');
             this._backdrop = this._initializeBackdrop();
+            this._scrollbar = new CFW_Scrollbar();
 
             this.$element.attr('data-cfw', 'modal');
 
@@ -3852,8 +4039,7 @@ if (typeof jQuery === 'undefined') {
 
             this.isShown = true;
 
-            this.checkScrollbar();
-            this.setScrollbar();
+            this._scrollbar.disable();
             this.$body.addClass('modal-open');
 
             this.escape();
@@ -3999,7 +4185,7 @@ if (typeof jQuery === 'undefined') {
             this.backdrop(function() {
                 $selfRef.$body.removeClass('modal-open');
                 $selfRef.resetAdjustments();
-                $selfRef.resetScrollbar();
+                $selfRef._scrollbar.reset();
                 $selfRef.$target
                     .CFW_mutateTrigger()
                     .CFW_trigger('afterHide.cfw.modal');
@@ -4103,10 +4289,12 @@ if (typeof jQuery === 'undefined') {
 
         adjustDialog : function() {
             var modalIsOverflowing = this.$target[0].scrollHeight > document.documentElement.clientHeight;
+            var scrollbarWidth = this._scrollbar.getScrollbarWidth();
+            var bodyIsOverflowing = this._scrollbar.isOverflowing();
 
             this.$target.css({
-                paddingLeft:  !this.bodyIsOverflowing && modalIsOverflowing ? this.scrollbarWidth : '',
-                paddingRight: this.bodyIsOverflowing && !modalIsOverflowing ? this.scrollbarWidth : ''
+                paddingLeft:  !bodyIsOverflowing && modalIsOverflowing ? scrollbarWidth : '',
+                paddingRight: bodyIsOverflowing && !modalIsOverflowing ? scrollbarWidth : ''
             });
         },
 
@@ -4115,77 +4303,6 @@ if (typeof jQuery === 'undefined') {
                 paddingLeft: '',
                 paddingRight: ''
             });
-        },
-
-        checkScrollbar : function() {
-            var rect = document.body.getBoundingClientRect();
-            this.bodyIsOverflowing = Math.round(rect.left + rect.right) < window.innerWidth;
-            this.scrollbarWidth = $.CFW_measureScrollbar();
-            this.scrollbarSide =  $('html').CFW_getScrollbarSide();
-        },
-
-        setScrollbar : function() {
-            var $selfRef = this;
-
-            if (this.bodyIsOverflowing) {
-                // Update fixed element padding
-                $(this.fixedContent).each(function() {
-                    $selfRef.setScrollbarAdjustment(this, 'padding', function(calculatedDim) { return calculatedDim + $selfRef.scrollbarWidth; });
-                });
-
-                // Update sticky element margin
-                $(this.stickyContent).each(function() {
-                    $selfRef.setScrollbarAdjustment(this, 'margin', function(calculatedDim) { return calculatedDim - $selfRef.scrollbarWidth; });
-                });
-
-                // Update body padding
-                this.setScrollbarAdjustment(document.body, 'padding', function(calculatedDim) { return calculatedDim + $selfRef.scrollbarWidth; });
-            }
-
-            this.$target
-                .on('touchmove.cfw.modal', this._scrollBlock.bind(this))
-                .CFW_trigger('scrollbarSet.cfw.modal');
-        },
-
-        resetScrollbar : function() {
-            var $selfRef = this;
-
-            // Restore fixed element padding
-            $(this.fixedContent).each(function() {
-                $selfRef.resetScrollbarAdjustment(this, 'padding');
-            });
-
-            // Restore sticky element margin
-            $(this.stickyContent).each(function() {
-                $selfRef.resetScrollbarAdjustment(this, 'margin');
-            });
-
-            // Restore body padding
-            this.resetScrollbarAdjustment(document.body, 'padding');
-
-            this.$target
-                .off('touchmove.cfw.modal')
-                .CFW_trigger('scrollbarReset.cfw.modal');
-        },
-
-        setScrollbarAdjustment : function(node, rule, callback) {
-            if (node !== document.body && window.innerWidth > node.clientWidth + this.scrollbarWidth) {
-                return;
-            }
-
-            var propName = rule + this.scrollbarSide.capitalize();
-            var actualDim = node.style[propName];
-            var calculatedDim = parseFloat(window.getComputedStyle(node)[propName]);
-            $(node).data('cfw.' + rule + '-dim', actualDim);
-            node.style[propName] = callback(calculatedDim) + 'px';
-        },
-
-        resetScrollbarAdjustment : function(node, rule) {
-            var savedDim = $(node).data('cfw.' + rule + '-dim');
-            if (typeof savedDim !== 'undefined') {
-                node.style[rule + this.scrollbarSide.capitalize()] = savedDim;
-                $(node).removeData('cfw.' + rule + '-dim');
-            }
         },
 
         _scrollBlock : function(e) {
@@ -4287,11 +4404,10 @@ if (typeof jQuery === 'undefined') {
             this.$element = null;
             this.$target = null;
             this.$dialog = null;
-            this.backdrop = null;
+            this._backdrop = null;
+            this._scrollbar = null;
             this.$focusLast = null;
             this.isShown = null;
-            this.scrollbarWidth = null;
-            this.scrollbarSide = null;
             this.fixedContent = null;
             this.ignoreBackdropClick = null;
             this.settings = null;
